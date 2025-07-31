86127a6afb7939d8487a9770d7ee591058bce3a9
swbisset <threedegreeswest@hotmail.com>
2025-07-31 10:22:53 +1000

    Updated splitFasta.py to be compatible with Python3

--- splitFasta.py
+++ splitFasta.py
@@ -1,96 +1,141 @@
 import csv
 import sys
 import argparse
-import time
 
-def readFile(file):
+def readFasta(infile):
+    global log
     try:
-        open(file)
+        open(infile)
     except IOError:
-        print "Error: cannot open %s. Please submit a valid file" % (file)
+        print("Error: Cannot open %s. Please submit a valid file name" % (infile))
         sys.exit()
     colList = []
-    mainList = []
-    #This just checks the file name of the file to decide whether or not to delimit by comma or tab-separated columns
-    if ".csv" in file:
-        print "Comma chosen as delimiter"
-        delStr = ','
+    fastaList = []
+    seq_str = ""
+    started = False
+    with open(infile) as r:
+        for line in r:
+            if line[0] == '>':
+                if not started:
+                    colList.append(line.rstrip('\n'))
+                    started = True
+                else:
+                    colList.append(seq_str)
+                    fastaList.append(colList[0:])
+                    colList[:] = []
+                    seq_str = ""
+                    colList.append(line.rstrip('\n'))
+                    #colList.append(line)
+            else:
+                #seq_str += line.rstrip('\n')
+                seq_str += line
+        colList.append(seq_str)
+        fastaList.append(colList[0:])
+    log_str = ("Number of sequences read in from %s: %s" % (infile, str(len(fastaList))))
+    log = ("%s\n%s" % (log, log_str))
+    if noisy:
+        print(log_str)
+    return fastaList
+
+def readFile(inFile):
+    global log
+    inList = []
+    with open(inFile) as r:
+        for line in r:
+            if len(line) > 1:
+                inList.append(line.strip('\n'))
+    log_str = ("Number of lines read in from %s: %s" % (inFile, str(len(inList))))
+    log = ("%s\n%s" % (log, log_str))
+    if noisy: 
+        print(log_str)
+    return inList
+
+def compareLists(fastaList, stringList):
+    global log
+    compList = []
+    includeList = []
+    excludeList = []
+    if isFasta:
+        for i in range(0, len(stringList)):
+            compList.append(stringList[i][0])
     else:
-        print "Tab chosen as delimiter"
-        delStr = '\t'
-    #This just reads in the file as a two-dimensional list
-    with open(file) as r:
-        reader = csv.reader(r, delimiter=delStr)
-        for row in reader:
-            for col in row:
-                colList.append(col)
-            mainList.append(colList[0:])
-            colList[:] = []
-    print "%s lines read in from %s" % (str(len(mainList)), file)
-    return mainList
+        compList = stringList
+    for i in range(0, len(fastaList)):
+        hasGene = False
+        for j in range(0, len(compList)):
+            if str(compList[j]) in str(fastaList[i][0]):
+                includeList.append(fastaList[i][0:])
+                hasGene = True
+                break
+        if not hasGene:
+            excludeList.append(fastaList[i][0:])
+    log_str = ("%s entries were included\n%s entries were excluded" % (str(len(includeList)), str(len(excludeList))))
+    log = ("%s\n%s" % (log, log_str))
+    if noisy:
+        print(log_str)
+    if exclude:
+        return excludeList
+    else:
+        return includeList
 
-def splitFile(fasta_list, info_list, c, i, split_str):
-    out_str = ""
-    count = 0
-    #This just reads through the table containing splitting information (info_list), and where
-    #a copy of the string (split_str) is found, the corresponding fasta ID is taken and
-    #searched against the initial fasta file (fasta_list)
-    for x in range(0, len(info_list)):
-        if split_str in str(info_list[x][i]):
-            read = str(info_list[x][c])
-            for y in range(0, len(fasta_list)):
-                if read in str(fasta_list[y]):
-                    out_str += "%s\n%s\n" % (str(fasta_list[y]), str(fasta_list[y+1]))
-                    count += 1
-                    break
-    print "%s reads out of %s found containg '%s' in column %s" % (str(count), str(len(fasta_list)/2), split_str, str(i+1))
-    return out_str
 
-def writeOutput(out_str, out_file):
-    with open(out_file, 'wb') as w:
-        w.write(out_str)
-    print "Output written to %s" % (out_file)
-
-#Begin main script here
-parser = argparse.ArgumentParser()
-parser.add_argument('fasta_file', help="Fasta file containing reads to be split")
-parser.add_argument('info_file', help="File in csv or tsv format which contains parameters for filtering/ splitting")
-parser.add_argument('split_string', help="String to filter or split reads by. Should appear in a column of info_file")
-parser.add_argument('-n', '--name', help="Column of info_file which contains names of reads in fasta_file. Default is 1")
-parser.add_argument('-i', '--info', help="Colum of info_file which contains splitting informaton. Default is 2")
-parser.add_argument('-o', '--output', help="Name of output file")
+# Main script 
+parser = argparse.ArgumentParser() 
+parser.add_argument('fasta_file', help="Fasta file to be read in")
+parser.add_argument('split_text', help="Text file containing entries to keep/ discard")
+parser.add_argument('-o', '--output', help="Output file to write to")
+parser.add_argument('-f', '--fasta', help="Use if comparing two fasta files (i.e. split_text is also a fasta)", action="store_true")
+parser.add_argument('-x', '--exclude', help="Exclude entries from split_text", action="store_true")
+parser.add_argument('-v', '--verbose', help="Set verbose", action="store_true")
 
 # Error catch 1: Make sure that some arguments have been supplied
 try:
     args = parser.parse_args()
 except SystemExit:
-    print "Please enter valid file type \ntype 'python splitFasta.py -h for help'"
+    print("Please enter valid file type \ntype 'python3 splitFasta.py -h for help'")
     sys.exit()
 
 args = parser.parse_args()
-starttime = time.time()
 
-# Make sure everything is behaving as expected
 if args.output:
     out_file = args.output
 else:
-    out_file = "%s_%s.fasta" % (args.fasta_file[:-6], args.split_string)
-    #If an output name is not supplied, then the original fasta file will be renamed with the
-    #splitting string within the name
+    out_file = ("%s_split.faa" % (args.fasta_file[:-4]))
 
-if args.name:
-    c = int(args.name) - 1
+logfile = ("%s.log" % (out_file[:-4]))
+log = "python3 splitFasta.py"
+
+exclude = False
+if args.exclude:
+    exclude = True
+
+isFasta = False
+if args.fasta:
+    isFasta = True
+
+noisy = False
+if args.verbose:
+    noisy = True
+
+# Start execution of script here
+fasta = readFasta(args.fasta_file)
+if isFasta:
+    strings=readFasta(args.split_text)
 else:
-    c = 0
+    strings = readFile(args.split_text)
+outList = compareLists(fasta, strings)
 
-if args.info:
-    i = int(args.info) - 1
-else:
-    i = 1
+# Write fasta file for output
+out_str = ("%s\n%s" % (str(outList[0][0]), str(outList[0][1])))
+for i in range(1, len(outList)):
+    out_str = ("%s\n%s\n%s" % (out_str, str(outList[i][0]), str(outList[i][1])))
 
-fasta = readFile(args.fasta_file)
-info = readFile(args.info_file)
-out_str = splitFile(fasta, info, c, i, args.split_string)
-writeOutput(out_str, out_file)
-#Just see how long the process took to run
-print "Process took %s seconds" % (str(time.time() - starttime))
+# Write output to file
+with open(out_file, 'w') as w:
+    w.write(out_str)
+if noisy:
+    print("Output written to %s" % (out_file))
+
+# Write log file
+with open(logfile, 'w') as l:
+    l.write(log)
